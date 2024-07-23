b667fe013cbb684dac1ed65da693981c67f068d1
siva6 <siva6@cj.net>
2024-07-23 13:10:58 +0900

    makePlayerList.js에 오류 메세지 추가

--- makePlayerList.js
+++ makePlayerList.js
@@ -1,12 +1,21 @@
 const fs = require('node:fs').promises;
 
+const mp3Path = './lib/data/player';
+const playerListPath = './lib/content/player.js';
+
 const ls = async directoryPath  =>  await fs.readdir(directoryPath);
 
 const run = async () => {
-	const files = await ls('./lib/data/player');
+	const files = await ls(mp3Path);
 	const players = files.map(file => parseInt(file.split('.')[0]));
-	console.log(players);
-	fs.writeFile('./lib/content/player.js', `const players = ${JSON.stringify(players)};`);
+	const playersJs = `const players = ${JSON.stringify(players)};`;
+
+	const playerJsFromFile = await fs.readFile(playerListPath, 'utf8');
+	if(playersJs === playerJsFromFile) return process.exit();
+
+
+	await fs.writeFile(playerListPath, `const players = ${JSON.stringify(players)};`);
+	process.exit(-1);
 };
 
 run();
